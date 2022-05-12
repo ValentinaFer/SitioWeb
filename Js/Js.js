300d5029f74acf3f91f332a8e5dead69d50711e1
ValentinaFer <quevedoants@gmail.com>
2022-05-12 15:02:09 -0300

    js checkbox, imgRandom

--- Js/Js.js
+++ Js/Js.js
@@ -1,32 +1,31 @@
 
-/*
-function ingredientText() {
-    
-    let ar = document.getElementById("i[]");
-    let aux = 0;
-    for (let i = 0; i < 5; i++){
-        if (ar[i] == true){
-            aux++;
+function ingredientText(){
+    let ingchecked = 0;
+    let ings = document.getElementById("ing");
+    let text = document.getElementById("text");
+
+    for (let i = 0; i < 11; i++){
+        if (ings.in[i].checked){
+            ingchecked++;
         }
     }
-    if (aux == 1){
+
+    if (ingchecked == 11){
         text.style.display = "block";
-        }else{
-            text.style.display = "none";
-        }
-
-    for (let i = 0; i < 5; i++) {
-        ing = document.getElementById("i"+1);
-
-        let text = document.getElementById("text");
-
-        if (ing.checked == true){
-            text.style.display = "block";
-        }else{
-            text.style.display = "none";
-        }
+    } else {
+        text.style.display = "none";
     }
+}
 
-}*/
+function imgRandom(){
 
+    let img_num = 3;
+    let random_n = Math.random();
+    let rand1 = Math.round((img_num-1)*random_n)+1;
+    img = new Array;
+    img[1] = "Css-images/cookies.png";
+    img[2] = "Css-images/gal.jfif";
+    img[3] = "Css-images/raspb.jfif";
+    document.miimagen.src = img[rand1];
 
+}
