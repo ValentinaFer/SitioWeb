1de391c1a9da770e4136db1b99419ec440308834
ValentinaFer <quevedoants@gmail.com>
2022-05-17 11:04:23 -0300

    comment section sim works, fixed some stuff
    missing date tho

--- Js/Js.js
+++ Js/Js.js
@@ -19,9 +19,9 @@ function ingredientText(){
 
 function imgRandom(){
 
-    let img_num = 3;
-    let random_n = Math.random();
-    let rand1 = Math.round((img_num-1)*random_n)+1;
+    var img_num = 3;
+    var random_n = Math.random();
+    var rand1 = Math.round((img_num-1)*random_n)+1;
     img = new Array;
     img[1] = "Css-images/cookies.png";
     img[2] = "Css-images/gal.jfif";
@@ -31,27 +31,26 @@ function imgRandom(){
 }
 
 function submitOrNat(){
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let comment = document.getElementById("comment").value;
-    let submit;
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var comment = document.getElementById("comment").value;
+    var submit;
+    var expresion = /^\w+@\w+(\.\w{3})$/;
 
     if (!name){
-        alert("Must enter a valid name");
+        //alert("Must enter a valid name");
+        document.getElementById("name").classList.add('error');
+    } else {
+        document.getElementById("name").classList.remove('error');
     }
-    if (!email){
-        alert("Must enter a valid email direction");
+    if (!expresion.test(email)){
+        //alert("Must enter a valid email direction");
+        document.getElementById("email").classList.add('error');
+    } else {
+        document.getElementById("email").classList.remove('error');
     }
     if (!comment){
-        alert("Must write a comment");
-    }
-
-    if (name && email && comment){
-        let father = document.getElementById("cs");
-        let div = document.createElement('div');
-        div.className = 'comment';
-        div.innerHTML = comment.value;
-        
+        //alert("Must write a comment");
     }
 
     let notify = document.getElementById("answer");
@@ -59,4 +58,22 @@ function submitOrNat(){
         alert("You will be notified when someone answers your comment!");
     }
 
+    if (name && email && comment){
+        alert("correct");
+        var father = document.getElementById("cs");
+        var div = document.createElement('div');
+        div.className = 'comment';
+        var user = document.createElement('p');
+        user.className = 'userName';
+        var c = document.createElement('p');
+        father.appendChild(div);
+        
+        user.innerHTML = name;
+        c.innerHTML = comment;
+        div.appendChild(user);
+        div.appendChild(c);
+        return false;
+    }
+
+    return false;
 }
