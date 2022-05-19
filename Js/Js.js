dd94adb09b4bb9f67f156cdb246e569f768bed17
ValentinaFer <quevedoants@gmail.com>
2022-05-19 17:58:53 -0300

    added slider

--- Js/Js.js
+++ Js/Js.js
@@ -1,4 +1,37 @@
 
+var imgh;
+var cont = 0;
+var imgs = [];
+
+function load(){
+    imgs[0] = "Css-images/cookies.png";
+    imgs[1] = "Css-images/gal.jfif";
+    imgs[2] = "Css-images/raspb.jfif";
+    imgh = document.getElementById("img-slide");
+    nextImg();  
+}
+
+function nextImg(){
+
+    imgh.src = imgs[cont];
+    if (cont < imgs.length-1) {
+        cont++;
+    } else {
+        cont = 0;
+    }
+}
+
+function prevImg(){
+
+    imgh.src = imgs[cont];
+    if (cont > 0){
+        cont--;
+    } else {
+        cont = imgs.length-1;
+    }
+
+}
+
 function ingredientText(){
     let ingchecked = 0;
     let ings = document.getElementsByName("in");
@@ -17,8 +50,8 @@ function ingredientText(){
     }
 }
 
+/*
 function imgRandom(){
-
     var img_num = 3;
     var random_n = Math.random();
     var rand1 = Math.round((img_num-1)*random_n)+1;
@@ -29,28 +62,35 @@ function imgRandom(){
     document.miimagen.src = img[rand1];
 
 }
+*/
 
 function submitOrNat(){
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var comment = document.getElementById("comment").value;
-    var submit;
+    var submit = [];
     var expresion = /^\w+@\w+(\.\w{3})$/;
 
     if (!name){
-        //alert("Must enter a valid name");
         document.getElementById("name").classList.add('error');
+        submit[0] = false;
     } else {
         document.getElementById("name").classList.remove('error');
+        submit[0] = true;
     }
     if (!expresion.test(email)){
-        //alert("Must enter a valid email direction");
         document.getElementById("email").classList.add('error');
+        submit[1] = false;
     } else {
         document.getElementById("email").classList.remove('error');
+        submit[1] = true;
     }
     if (!comment){
-        //alert("Must write a comment");
+        document.getElementById("comment").classList.add('error');
+        submit[2] = false;
+    } else {
+        document.getElementById("comment").classList.remove('error');
+        submit[2] = true;
     }
 
     let notify = document.getElementById("answer");
@@ -58,7 +98,10 @@ function submitOrNat(){
         alert("You will be notified when someone answers your comment!");
     }
 
-    if (name && email && comment){
+    let aux = 0;
+    for (let index = 0; index < submit.length; index++) {(submit[index] ? aux++: aux)};
+
+    if (aux === submit.length){
         alert("correct");
         var father = document.getElementById("cs");
         var div = document.createElement('div');
@@ -67,7 +110,6 @@ function submitOrNat(){
         user.className = 'userName';
         var c = document.createElement('p');
         father.appendChild(div);
-        document.get
         user.innerHTML = name;
         c.innerHTML = comment;
         div.appendChild(user);
